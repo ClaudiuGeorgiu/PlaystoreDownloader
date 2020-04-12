38485e7d6b709a900d43b260264ac1b0f8a1646a
Auto Black Formatter <claudiugeorgiu@users.noreply.github.com>
2020-04-12 16:44:58 +0000

    Format code with Black [auto]

--- download.py
+++ download.py
@@ -52,7 +52,7 @@ def get_cmd_args(args: list = None):
         metavar="CREDENTIALS",
         default=credentials_default_location,
         help="The path to the JSON configuration file containing the store "
-             'credentials. By default the "credentials.json" file will be used',
+        'credentials. By default the "credentials.json" file will be used',
     )
     parser.add_argument(
         "-o",
@@ -61,8 +61,8 @@ def get_cmd_args(args: list = None):
         metavar="FILE",
         default=downloaded_apk_default_location,
         help="The path where to save the downloaded .apk file. By default the file "
-             'will be saved in a "Downloads/" directory created where this script '
-             'is run',
+        'will be saved in a "Downloads/" directory created where this script '
+        "is run",
     )
     parser.add_argument(
         "-t",
