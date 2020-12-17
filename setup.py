55d0e770e0729b0cf303a240bd7f2b71869b1491
David Sevcik <david@MacBook-Pro.local>
2020-12-17 15:27:26 +0100

    TRIVIAL - fix requirements bug in setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,6 @@
 import setuptools
 from playstore_downloader.version import VERSION
 
-def parse_requirements(filename):
-    """ load requirements from a pip requirements file """
-    lineiter = (line.strip() for line in open(filename))
-    return [line for line in lineiter if line and not line.startswith("#")]
-
-install_reqs = parse_requirements("requirements.txt")
 
 setuptools.setup(
      name='playstore-downloader',  
@@ -16,7 +10,15 @@ setuptools.setup(
      url="https://github.com/wandera/PlaystoreDownloader",
      packages=setuptools.find_packages(),
      package_dir={'PlaystoreDownloader': 'PlaystoreDownloader'},
-     install_requires=install_reqs,
+     install_requires=[
+        'Flask==1.1.2',
+        'Flask-SocketIO==4.3.1',
+        'protobuf==3.13.0',
+        'pycryptodome==3.9.8',
+        'pytest-cov==2.10.1',
+        'requests==2.24.0',
+        'tqdm==4.50.2'
+     ],
      classifiers=[
          "Programming Language :: Python :: 3",
          "Operating System :: POSIX :: Linux",
